44ee34cc1241676407870301193e70012b8ba9c5
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-10-04 18:19:58 -0500

    coding

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,46 @@
 // @TODO: YOUR CODE HERE!
+
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3.select(".chart")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Import Data
+let file= "assets/data/data.csv"
+d3.csv(file).then(function(healthData){
+    console.log(healthData);
+    healthData.forEach(function(data){
+        data.age= +data.age;
+        data.smokes= +data.smokes;
+    });
+    // Step 2: Create scale functions
+    // ==============================
+    var xLinearScale = d3.scaleLinear()
+      .domain([20, d3.max(healthData, d => d.age)])
+      .range([0, width]);
+
+    var yLinearScale = d3.scaleLinear()
+      .domain([0, d3.max(healthData, d => d.smokes)])
+      .range([height, 0]);
+});
+    
+
+
+   
